a3d2bf416d46fcdf22ad5ffeaf86048f52c32e04
Damien <damien@fluidsyntax.com>
2014-01-14 10:38:38 +0100

    Disable logging during tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,10 @@ VCR.configure do |c|
   c.hook_into :webmock # or :fakeweb
 end
 
+# Disable STDOUT logging during tests
+ApiModel.send :remove_const, :Log
+ApiModel::Log = Logger.new('/dev/null')
+
 RSpec.configure do |config|
 
   # Reset any config changes after each spec
